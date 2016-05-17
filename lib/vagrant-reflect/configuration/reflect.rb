9e25f50f624ccdb51b915d55a5852f6436e4bd63
Jason Woods <devel@jasonwoods.me.uk>
2016-05-17 19:13:32 +0100

    rubocop configuration

--- lib/vagrant-reflect/configuration/reflect.rb
+++ lib/vagrant-reflect/configuration/reflect.rb
@@ -1,7 +1,7 @@
 module VagrantReflect
   module Configuration
+    # Configuration object for vagrant-reflect
     class Reflect < Vagrant.plugin('2', :config)
-
       attr_accessor :show_sync_time
 
       def initialize
@@ -12,13 +12,13 @@ module VagrantReflect
         @show_sync_time = 0 if @show_sync_time == UNSET_VALUE
       end
 
-      def validate(machine)
+      def validate(_)
         errors = _detected_errors
-        if !!:show_sync_time == :show_sync_time
-          errors << "show_sync_time must be TRUE or FALSE"
+        if show_sync_time != true && show_sync_time != false
+          errors << 'show_sync_time must be TRUE or FALSE'
         end
 
-        { "reflect" => errors }
+        { 'reflect' => errors }
       end
     end
   end
